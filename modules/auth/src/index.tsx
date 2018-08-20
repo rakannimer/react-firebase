8ae58a74f81e5d20c531bd14b8d925207e25042f
rakannimer <rakannimer@gmail.com>
2018-08-20 17:00:38 +0300

    import only firebase/auth

--- modules/auth/src/index.tsx
+++ modules/auth/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { renderAndAddProps } from "render-and-add-props";
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 import get from "lodash.get";
 
 import { initializeFirebaseApp } from "./initialize-firebase-app";
