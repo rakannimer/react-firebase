8ed126e9db6c96d6c992488165d68768e5926106
rakannimer <rakannimer@gmail.com>
2018-08-22 13:16:45 +0300

    [firestore] Add write components. Mutation-BatchedWrite-Transaction

--- modules/firestore/src/index.tsx
+++ modules/firestore/src/index.tsx
@@ -1,3 +1,7 @@
 export { FirestoreCollection } from "./components/FirestoreCollection";
 export { FirestoreDocument } from "./components/FirestoreDocument";
 export { FirestoreProvider } from "./components/FirestoreProvider";
+
+export { FirestoreMutation } from "./components/FirestoreMutation";
+export { FirestoreBatchedWrite } from "./components/FirestoreBatchedWrite";
+export { FirestoreTransaction } from "./components/FirestoreTransaction";
