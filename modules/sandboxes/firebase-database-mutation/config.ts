a2d11c7a03027b5777ae5ad5db184f3f8581d01f
rakannimer <rakannimer@gmail.com>
2018-08-26 13:46:40 +0300

    [sandboxes] Add Firebase Database Example.

---
+++ modules/sandboxes/firebase-database-mutation/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+  // DO NOT USE THESE CREDENTIALS ! THEY ARE HERE TO HELP IN THE LEARNING PROCESS.
+  // ANY AND ALL DATA ON THAT DOMAIN IS SUBJECT TO CHANGE AND REMOVAL AT ANY TIME
+  // THIS ACCOUNT IS ALSO ON THE FREE PLAN AND IS SUBJECT TO RESTRICTIONS !
+  apiKey: "AIzaSyBIEgdW0FGGxQZ7bbbqIhzmiVAk7nnJB14",
+  authDomain: "react-firebase-baecd.firebaseapp.com",
+  databaseURL: "https://react-firebase-baecd.firebaseio.com",
+  projectId: "react-firebase-baecd",
+  storageBucket: "react-firebase-baecd.appspot.com",
+  messagingSenderId: "6611069556"
+};
---
+++ modules/sandboxes/firebase-database-mutation/index.tsx
@@ -0,0 +1,92 @@
+import * as React from "react";
+import { render } from "react-dom";
+import firebase from "firebase/app";
+import {
+  FirebaseDatabaseProvider,
+  FirebaseDatabaseNode,
+  FirebaseDatabaseMutation
+} from "@react-firebase/database";
+import { config } from "./config";
+
+const styles = {
+  fontFamily: "sans-serif"
+};
+
+const s = (a: any) => JSON.stringify(a, null, 2);
+
+const collectionPath = "TEST_NAMESPACE/user_bookmarks/";
+const testDocValue = {
+  nowOnCli: Date.now(),
+  nowOnServer: firebase.database.ServerValue.TIMESTAMP,
+  some: "data"
+};
+
+export type AppState = {
+  keys: string[];
+};
+
+class App extends React.Component<any, AppState> {
+  state = {
+    keys: []
+  };
+  render() {
+    return (
+      <div style={styles}>
+        <FirebaseDatabaseProvider firebase={firebase} {...config}>
+          <div>
+            <FirebaseDatabaseMutation path={collectionPath} type="push">
+              {({ runMutation }) => (
+                <button
+                  data-testid="add-document"
+                  onClick={async () => {
+                    const { key } = await runMutation(testDocValue);
+                    if (key === null || typeof key === "undefined") return;
+                    this.setState(state => ({
+                      keys: [...state.keys, key]
+                    }));
+                  }}
+                >
+                  add-document-with-generated-key
+                </button>
+              )}
+            </FirebaseDatabaseMutation>
+            {this.state.keys.map(key => (
+              <React.Fragment key={key}>
+                <FirebaseDatabaseNode path={`${collectionPath}/${key}`}>
+                  {({ value }) => <pre>{s(value)}</pre>}
+                </FirebaseDatabaseNode>
+                <FirebaseDatabaseMutation
+                  path={`${collectionPath}/${key}`}
+                  type="set"
+                >
+                  {({ runMutation }) => (
+                    <button
+                      onClick={async () => {
+                        await runMutation(null);
+                        this.setState((state: any) => ({
+                          ...state,
+                          keys: [
+                            ...state.keys.filter(
+                              (currentKey: any) => currentKey !== key
+                            )
+                          ]
+                        }));
+                      }}
+                    >
+                      delete-generated-document
+                    </button>
+                  )}
+                </FirebaseDatabaseMutation>
+              </React.Fragment>
+            ))}
+            {this.state.keys.length > 0 && (
+              <div data-testid="add-document-result">{s(this.state.keys)}</div>
+            )}
+          </div>
+        </FirebaseDatabaseProvider>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
