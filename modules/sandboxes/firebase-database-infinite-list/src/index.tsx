b2d5230d5d0ccb57ee3bbcb3cdb566e60b9441c6
rakannimer <rakannimer@gmail.com>
2018-08-28 15:25:56 +0300

    [sandboxes] Add firebase ( auth | database | firestore ) import to examples.

--- modules/sandboxes/firebase-database-infinite-list/src/index.tsx
+++ modules/sandboxes/firebase-database-infinite-list/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
 import firebase from "firebase/app";
+import "firebase/database";
 import {
   FirebaseDatabaseProvider,
   FirebaseDatabaseNode
--- modules/sandboxes/firestore-collection-example/src/index.tsx
+++ modules/sandboxes/firestore-collection-example/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { render } from "react-dom";
 
 import firebase from "firebase/app";
-
+import "firebase/firestore";
 import {
   FirestoreProvider,
   FirestoreCollection
