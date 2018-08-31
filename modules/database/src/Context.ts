1db4e33c5757a110d5f6d6bfa65fa5a130336115
BilalYazbek <Bilal@et3arraf.com>
2018-08-31 12:37:48 +0300

    add lodash memioze

--- modules/database/src/Context.ts
+++ modules/database/src/Context.ts
@@ -1,7 +1,7 @@
 
 import * as React from "react";
 import { FirebaseDatabaseProviderState, FirebaseQuery } from "./types";
-import { memoize } from 'lodash'
+import memoize from 'lodash.memoize'
 export const firebaseDatabaseDefaultContext = {
   listenTo: ({ path }: FirebaseQuery) => { },
   stopListeningTo: (path: string) => { },
