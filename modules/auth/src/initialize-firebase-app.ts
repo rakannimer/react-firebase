febddd1146e81275c45064ca2c97c1bc9b3d238d
rakannimer <rakannimer@gmail.com>
2018-09-11 10:50:25 +0300

    [auth] 0.2.7
    - Remove warning if app initialized twice.

--- modules/auth/src/initialize-firebase-app.ts
+++ modules/auth/src/initialize-firebase-app.ts
@@ -21,8 +21,5 @@ export const initializeFirebaseApp = ({
     if (err.code !== "app/duplicate-app") {
       throw err;
     }
-    console.warn(
-      "App has already been initialized, make sure you're not rendering FirebaseAuthProvider twice. If this is intended, you can safely ignore."
-    );
   }
 };
